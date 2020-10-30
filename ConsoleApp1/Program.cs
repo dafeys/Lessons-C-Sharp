4fb915eb54ff4a022b5c8f246bf11a44e1f0a6ef
Ivan F <dafeys@protonmail.com>
2020-10-30 19:22:24 +0200

    1st

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1, str2;
+            int a, b, result;
+            str1 = "10";
+            str2 = "7";
+
+
+
+            a = Convert.ToInt32(str1);
+            b = Convert.ToInt32(str2);
+            result = a + b;
+            Console.WriteLine("Output = " + result);
+            string str = "1.9";
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+
+            double d = Convert.ToDouble(str, numberFormatInfo);
+            Console.WriteLine("Double = " + d);
+            
+            //str = Console.ReadLine();
+        }
+    }
+}
